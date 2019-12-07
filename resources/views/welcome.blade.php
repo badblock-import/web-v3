203b0a5d645c4e8851779ee68ed66015c650d492
MrValentin <c4mtxlegetteur@gmail.com>
2019-12-07 11:07:38 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel c'est bien
                 </div>
 
                 <div class="links">
